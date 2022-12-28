d825afa765a39af292b1f2e0b2dd41c520cbe856
Bog0sso <sambe2000@hotmail.com>
2022-12-28 14:15:02 +0000

    initial: spring boot project set - Account class as record

---
+++ moneytransfer/src/main/java/com/eknox/moneytransfer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.eknox.moneytransfer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MoneytransferApplication.class);
+	}
+
+}
---
+++ moneytransfer/src/main/java/com/eknox/moneytransfer/entities/Account.java
@@ -0,0 +1,7 @@
+package com.eknox.moneytransfer.entities;
+
+/**
+ * Account
+ */
+public record Account() {
+}
