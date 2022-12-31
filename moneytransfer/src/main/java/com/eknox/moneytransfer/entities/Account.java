ec3d8222fa40f562ca56e02e30863dc51bc68a6f
mariemeb8 <120035917+mariemeb8@users.noreply.github.com>
2022-12-31 16:12:49 +0000

    Update Account.java

--- moneytransfer/src/main/java/com/eknox/moneytransfer/entities/Account.java
+++ moneytransfer/src/main/java/com/eknox/moneytransfer/entities/Account.java
@@ -3,5 +3,24 @@ package com.eknox.moneytransfer.entities;
 /**
  * Account
  */
-public record Account() {
+ 
+public class Account() 
+{
+ private double amount;
+ private int numcompte;
+ Account(){}
+ Account(double amount)
+ {
+   this.amount = amount;
+ }
+ 
+ public double getAmount()
+ {
+   return amout;
+ }
+ public static void Afficher()
+ {
+  Account cpt = new Account(0.0);
+  System.out.println("le solde est de : " +cpt.getAmount() + "FCFA");
+ } 
 }
