555a59b0d33825a132778c7cb37e803907b5a513
Habydev <120279688+Habydev@users.noreply.github.com>
2023-01-14 18:33:22 +0100

    Update Client.java

--- moneytransfer/src/main/java/com/eknox/moneytransfer/entities/Client.java
+++ moneytransfer/src/main/java/com/eknox/moneytransfer/entities/Client.java
@@ -1,3 +1,16 @@
 public class Client extends User {
     public transfererArgent(double montant,idCompte)
+        
+        TransfereArgent public nul() {
+        long double montant;  
+System.out.println(« Entrez le montant que vous souhaitez transferer: « );
+        amt = sc.nextLong();  
+        if (balance >= amt) {  
+solde = solde - amt;
+System.out.println(« Solde après transferer: " + solde);
+        } autre 
+        {  
+System.out.println(« Votre solde est inférieur à " + double montant + « \tÉchec de la transaction...!! » );
+        }  
+    }  
 }
