72bcdba3341b0556e1703cff73f1e2fcbfb3fd6f
AFarmaK16 <kanefarma16@gmail.com>
2023-01-15 23:17:56 +0000

    create transaction-form

---
+++ e-knox/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TransactionForm from './Components/TransactionForm';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <TransactionForm/>
+    </div>
+  );
+}
+
+export default App;
