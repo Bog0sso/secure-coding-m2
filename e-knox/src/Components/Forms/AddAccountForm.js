d301f8fb8d4b59e4ffb7b03135a8c8b400a1f5f5
AFarmaK16 <kanefarma16@gmail.com>
2023-02-25 09:34:18 +0000

    added registration form

--- e-knox/src/Components/Forms/AddAccountForm.js
+++ e-knox/src/Components/Forms/AddAccountForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-const AddAccountForm = () => {
+import { Form, FormGroup, Modal } from "react-bootstrap";
+const AddAccountForm = (props) => {
   const {
     register,
     handleSubmit,
@@ -15,91 +16,214 @@ const AddAccountForm = () => {
   };
 
   return (
-    // <div>numDestinataire, Status status, double montant, Devise devise,</div>
-    <div className="container">
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
       <div className="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
-        <div className="col-xl-4 col-lg-8 col-md-7 mx-auto">
-          <div className="card z-index-0">
-            <div className="card-header card-header-tabs card-header-primary">
-              <h4 className="card-title ">Ajout d'un compte</h4>
-            </div>
-            <div className="card-body">
-              <form onSubmit={handleSubmit(submitAction)}>
-                <div className="form-group">
-                  <label>Login </label>
-                  <input
-                    className={`form-control ${
-                      errors.login ? "is-invalid" : ""
-                    }`}
-                    type="text"
-                    name="login"
-                    {...register("login", {
-                      required: true,
-                      pattern: /^[a-zA-Z]+$/,
-                    })}
-                  />
-                  {errors.login && (
-                    <span className="text text-danger">{ONLY_LETTERS}</span>
-                  )}
-                </div>
-                <br />
+        <div className="col-xl-8 col-lg-8 col-md-7 mx-auto">
+          {/* <div className=" card"> */}
+          <Modal.Header
+            className="modal-header card-header-tabs card-header-primary"
+            closeButton
+          >
+            <Modal.Title className="card-title ">
+              <h4>Creation Compte</h4>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="card-body">
+            <Form onSubmit={handleSubmit(submitAction)}>
+              <FormGroup className="form-group">
+                <label>Nom</label>
+                <input
+                  className={`form-control ${errors.nom ? "is-invalid" : ""}`}
+                  type="text"
+                  name="nom"
+                  {...register("nom", {
+                    required: true,
+                    pattern: /^[a-zA-Z]+$/,
+                  })}
+                />
+                {errors.nom && (
+                  <span className="text text-danger">
+                    Ce champs ne doit contenir que des lettres
+                  </span>
+                )}
+              </FormGroup>
+              <FormGroup className="form-group">
+                <label>Prenom : </label>
+                <input
+                  className={`form-control ${
+                    errors.prenom ? "is-invalid" : ""
+                  }`}
+                  type="text"
+                  name="prenom"
+                  {...register("prenom", {
+                    required: true,
+                    pattern: /^[a-zA-Z]+$/,
+                  })}
+                />
+                {errors.prenom && (
+                  <span className="text text-danger">
+                    Ce champs ne doit contenir que des lettres
+                  </span>
+                )}
+              </FormGroup>
+              <FormGroup className="form-group">
+                <label>Date de naissamce:</label>
+                <input
+                  className={`form-control ${errors.dateN ? "is-invalid" : ""}`}
+                  type="date"
+                  name="dateN"
+                  {...register("dateN", {
+                    required: true,
+                  })}
+                />
+                {errors.dateN && (
+                  <span className="text text-danger">Date invalide</span>
+                )}
+              </FormGroup>
 
-                <div className="form-group">
-                  <label>Password </label>
-                  <input
-                    className={`form-control ${
-                      errors.password ? "is-invalid" : ""
-                    }`}
-                    type="text"
-                    name="password"
-                    {...register("password", {
-                      required: true,
-                      pattern: /[^a-zA-Z0-9]+$/,
-                      minLength: 8,
-                    })}
-                  />
-                  {errors.password && (
-                    <span className="text text-danger">
-                      Le mot de passe doit contenir au moins 8 characteres
-                      combiner à des chiffres et symboles
-                    </span>
-                  )}
-                </div>
-                <br />
+              <FormGroup className="form-group">
+                <label>Lieu de naissance</label>
+                <input
+                  className={`form-control ${errors.lieuN ? "is-invalid" : ""}`}
+                  type="text"
+                  name="lieuN"
+                  {...register("lieuN", {
+                    required: true,
+                    pattern: /^[a-zA-Z]+$/,
+                  })}
+                />
+                {errors.lieuN && (
+                  <span className="text text-danger">Saisie invalide</span>
+                )}
+              </FormGroup>
+              <FormGroup className="form-group">
+                <label>Adresse</label>
+                <input
+                  className={`form-control ${
+                    errors.adresse ? "is-invalid" : ""
+                  }`}
+                  type="text"
+                  name="adresse"
+                  {...register("adresse", {
+                    required: true,
+                    pattern: /^[a-zA-Z]+$/,
+                  })}
+                />
+                {errors.adresse && (
+                  <span className="text text-danger">Adresse invalide</span>
+                )}
+              </FormGroup>
+              <FormGroup className="form-group">
+                <label>Telephone</label>
+                <input
+                  className={`form-control ${
+                    errors.numero ? "is-invalid" : ""
+                  }`}
+                  type="number"
+                  name="numero"
+                  {...register("numero", {
+                    required: true,
+                    pattern: /^[0-9]+$/,
+                  })}
+                />
+                {errors.numero && (
+                  <span className="text text-danger">*Numero invalide</span>
+                )}
+              </FormGroup>
 
-                <div className="form-group">
-                  <label>Solde:</label>
-                  <input
-                    className={`form-control ${
-                      errors.solde ? "is-invalid" : ""
-                    }`}
-                    type="number"
-                    name="solde"
-                    {...register("solde", {
-                      required: true,
-                      pattern: /^[0-9]+$/,
-                      min: 1,
-                    })}
-                  />
-                  {errors.solde && (
-                    <span className="text text-danger">Solde invalide</span>
-                  )}
-                </div>
-                <br />
+              <FormGroup className="form-group">
+                <label>Email</label>
+                <input
+                  className={`form-control ${errors.email ? "is-invalid" : ""}`}
+                  type="email"
+                  name="email"
+                  {...register("email", {
+                    required: true,
+                    pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  })}
+                />
+                {errors.email && (
+                  <span className="text text-danger">Email invalide</span>
+                )}
+              </FormGroup>
+              <FormGroup className="form-group">
+                <label>Login </label>
+                <input
+                  className={`form-control ${errors.login ? "is-invalid" : ""}`}
+                  type="text"
+                  name="login"
+                  {...register("login", {
+                    required: true,
+                    pattern: /^[a-zA-Z0-9_-]{3,20}$/,
+                  })}
+                />
+                {errors.login && (
+                  <span className="text text-danger">
+                    Le login doit contenir au moins 3 caracteres
+                  </span>
+                )}
+              </FormGroup>
+              <br />
 
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="btn btn-primary"
-                >
-                  Soummettre
-                </button>
-              </form>
-            </div>
-          </div>
+              <FormGroup className="form-group">
+                <label>Password </label>
+                <input
+                  className={`form-control ${
+                    errors.password ? "is-invalid" : ""
+                  }`}
+                  type="password"
+                  name="password"
+                  {...register("password", {
+                    required: true,
+                    pattern:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,20}$/,
+                    minLength: 8,
+                  })}
+                />
+                {errors.password && (
+                  <span className="text text-danger">
+                    Le mot de passe doit contenir au moins 8 caractéres combiner
+                    à des chiffres et symboles(!@#$%^&*()_+)
+                  </span>
+                )}
+              </FormGroup>
+              <br />
+
+              <FormGroup className="form-group">
+                <label>Solde:</label>
+                <input
+                  className={`form-control ${errors.solde ? "is-invalid" : ""}`}
+                  type="number"
+                  name="solde"
+                  {...register("solde", {
+                    required: true,
+                    pattern: /^[0-9]+$/,
+                    min: 1,
+                  })}
+                />
+                {errors.solde && (
+                  <span className="text text-danger">Solde invalide</span>
+                )}
+              </FormGroup>
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="btn btn-primary"
+              >
+                Submit
+              </button>
+            </Form>
+          </Modal.Body>
+          {/* </div> */}
         </div>
       </div>
-    </div>
+    </Modal>
   );
 };
 export default AddAccountForm;
--- e-knox/src/pages/admin/Accounts.js
+++ e-knox/src/pages/admin/Accounts.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AddAccountForm from "../../Components/Forms/AddAccountForm";
+import { BsPlus } from "react-icons/bs";
 
 const AccountsPage = () => {
+  const [createAccountmodalShow, setcreateAccountModalShow] = useState(false);
   return (
     <div>
       <h1>Account Page</h1>
-      <AddAccountForm />
+      <div className="logo-circle2 icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+        <span
+          className="text-white"
+          onClick={() => setcreateAccountModalShow(true)}
+        >
+          <i className="opacity-10">
+            <BsPlus />
+          </i>
+        </span>
+      </div>
+      <AddAccountForm
+        show={createAccountmodalShow}
+        onHide={() => setcreateAccountModalShow(false)}
+      />
     </div>
   );
 };
